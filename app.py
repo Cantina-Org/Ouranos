a87b113d9e7b16e1280f777805d36952fbac126f
matyu <degueurce.mathieu@gmail.com>
2023-08-09 22:49:03 +0200

    - Création de la page install_nephelees.html et la fonction du même nom pour gérer les configs des installations. - Ajout d'un SVG du logo dans .idea pour l'image dans pycharm

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from Utils.step_1 import createDatabaseStep1
 
 app = Flask(__name__)
 db = None
+toolToInstall = []
 
 
 @app.route('/')
@@ -29,10 +30,16 @@ def step1():
 
 @app.route('/step2', methods=["POST", "GET"])
 def step2():
+    global toolToInstall
     if request.method == "POST":
         try:
             # Check value pour savoir quelle étape afficher ensuite.
-            return redirect(url_for('step3'))
+            print(request.form["nephelees"])
+            if request.form['nephelees'] == "on":
+                toolToInstall.append({"name": "nephelees", "config": {}})
+            if request.form['hermes'] == "on":
+                toolToInstall.append({"name": "hermes", "config": {}})
+            return redirect(url_for('install_nephelees'))
         except Exception as e:
             print(e)
             return redirect(url_for('step2', error="error_db"))
@@ -40,9 +47,22 @@ def step2():
         return render_template('step2.html')
 
 
+@app.route('/step2/config/nephelees', methods=['POST', 'GET'])
+def install_nephelees():
+    if request.method == "POST":
+        print("")
+    else:
+        print(toolToInstall)
+        for item in toolToInstall:
+            if item["name"] == "nephelees":
+                return render_template('install_nephelees.html')
+
+        return redirect(url_for("step3"))
+
+
 @app.route('/step3')
 def step3():
-    return ':0'
+    return toolToInstall
 
 
 if __name__ == '__main__':
