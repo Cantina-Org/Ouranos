472a1d57f09e594c312461133e82c78ab195c18d
matyu <degueurce.mathieu@gmail.com>
2023-08-25 17:22:42 +0200

    - fin de la step3 d'Hermes

--- Utils/step3.py
+++ Utils/step3.py
@@ -25,7 +25,6 @@ def install_nephelees_back(db, step2data: dict, step1data: dict):
     with open(step2data['custom_path'] + '/Nephelees/config.json', "w") as outfile:
         outfile.write(dumps(json_data, indent=4))
 
-    system("touch /etc/systemd/system/cantina-nephelees.service")
     system(f"""echo '[Unit]
     Description=Cantina Néphélées
     [Service]
@@ -50,10 +49,40 @@ def install_hermes_back(db, step2data: dict, step1data: dict):
         return "Node.js est installé, mais une erreur s'est produite lors de l'exécution de la commande."
 
     db.insert("""CREATE DATABASE IF NOT EXISTS cantina_hermes""")
-    db.insert("""INSERT INTO cantina_hermes.domain(name, fqdn) VALUES (%s, %s)""", ("olympe",
+    db.insert("""CREATE TABLE IF NOT EXISTS cantina_hermes.messages_stats(author TEXT, public_message BIGINT, 
+    private_message BIGINT, last_messages DATE NOT NULL DEFAULT current_timestamp)""")
+    db.insert("""INSERT INTO cantina_administration.domain(name, fqdn) VALUES (%s, %s)""", ("olympe",
                                                                                             step2data["web_addr"]))
 
     system(f"cd {step2data['custom_path']} && git clone https://github.com/Cantina-Org/Hermes.git")
+    system(f"cd {step2data['custom_path']}/Hermes && mkdir ./server/messages && "
+           "mkdir ./server/messages/private-messages")
+    system(f"cd {step2data['custom_path']}/Hermes && npm i")
+
+    json_data = {
+        "database": [{
+            "database_username": step1data['username_db'],
+            "database_password": step1data['password_db'],
+            "database_addresse": step1data['address_db'],
+            "database_port": step1data['port_db']
+        }],
+        "port": 3002
+    }
+
+    with open(step2data['custom_path'] + '/Hermes/server/config.json', "w") as outfile:
+        outfile.write(dumps(json_data, indent=4))
+
+    system(f"""echo '[Unit]
+        Description=Cantina Hermes
+        [Service]
+        User=cantina
+        WorkingDirectory={step2data['custom_path']}/Hermes/server
+        ExecStart=python3 app.py
+        [Install]
+        WantedBy=multi-user.target' >> /etc/systemd/system/cantina-hermes.service""")
+    system(f"chown cantina:cantina {step2data['custom_path']}/*/*/*/*")
+    system("systemctl enable cantina-hermes")
+    system("systemctl start cantina-hermes")
 
 
 def install_olympe_back(db, step2data: dict, step1data: dict):
