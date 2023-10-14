cc696e7db1a2ccd728ded79ba66940329c3e6c02
mathieu <degueurce.mathieu@gmail.com>
2023-10-14 22:10:06 +0200

    edit

--- install_nephelees.py
+++ install_nephelees.py
@@ -12,10 +12,10 @@ print('''  ______     ___      .__   __. .___________. __  .__   __.      ___
 
 print("Bienvenue dans l'installateur de Cantina Néphélees!")
 
-db_data["username"] = input("Quelle est le nom d'utilisateur de la base de donnée : ")
-db_data["password"] = input("Quelle est le mots de passe de la base de donnée : ")
-db_data["address"] = input("Quelle est l'addresse de la base de donnée : ")
-db_data["port"] = int(input("Quelle est le port d'accès de la base de donnée: "))
+db_data["username"] = 'cantina'  # input("Quelle est le nom d'utilisateur de la base de donnée : ")
+db_data["password"] = 'LeMdPDeTest'  # input("Quelle est le mots de passe de la base de donnée : ")
+db_data["address"] = '127.0.0.1'  # input("Quelle est l'addresse de la base de donnée : ")
+db_data["port"] = 3306  # int(input("Quelle est le port d'accès de la base de donnée: "))
 
 print(db_data)
 database = DataBase(host=db_data["address"], port=db_data["port"], user=db_data["username"],
@@ -24,3 +24,27 @@ try:
     database.connection()
 except ConnectionRefusedError:
     exit('Une erreur est survenue lors de la connexion à MariaDB/MySQL!')
+
+data = database.select('SHOW DATABASES')
+existing_instance = False
+
+for db in data:
+    if db[0] == 'cantina_administration':
+        existing_instance = True
+        break
+    else:
+        existing_instance = False
+
+print("Une instance de Cantina a été retrouvé dans la base de données.")
+wipe_db = input("Voulez vous réutiliser cette base de donnée? (Y/N) ")
+
+while wipe_db != 'Y' or wipe_db != 'N':
+    wipe_db = input("Voulez vous réutiliser cette base de donnée? (Y/N) ")
+
+if wipe_db == 'Y':
+    print('Suppression de la base de donnée cantina_administration...')
+    database.insert('''DROP DATABASE cantina_administration''', ())
+    print('Création de la nouvelle base de donnée cantina_administration...')
+
+else:
+    print('')
